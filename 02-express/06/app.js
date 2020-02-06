c72ff7c7f982e74291fe61142a40c38ab1c5e44c
JoshuaHolloway <jhollow6@asu.edu>
2020-02-06 12:11:25 -0600

    Adding a 404 error page

--- 02-express/06/app.js
+++ 02-express/06/app.js
@@ -14,6 +14,7 @@ const shop_routes = require('./routes/shop.js');
 app.use(shop_routes);
 //===========================
 // handle errors:
+// -if no path has been hit yet, then throw error
 app.use((req, res, next) => {
   console.log('JOSH: ERROR!!!!');
   console.log('JOSH: ERROR!!!!');
