79b1015d97c242d23385f9f34e6113c0748cbe2d
JoshuaHolloway <jhollow6@asu.edu>
2020-02-07 13:28:37 -0600

    Serving files statically

--- 02-express/12/app.js
+++ 02-express/12/app.js
@@ -7,6 +7,8 @@ const app = express();
 //===========================
 // register parser
 app.use(body_parser.urlencoded({ extended: false }));
+
+// [02-12] Static file paths will be forwarded here:
 app.use(express.static(path.join(__dirname, 'public')));
 //===========================
 const admin_routes = require('./routes/admin'); //.js
@@ -17,17 +19,12 @@ app.use(shop_routes);
 //===========================
 // handle errors:
 app.use((req, res, next) => {
-  console.log('JOSH: ERROR!!!!');
-  console.log('JOSH: ERROR!!!!');
-  console.log('JOSH: ERROR!!!!');
-  console.log('JOSH: ERROR!!!!');
-  console.log('JOSH: ERROR!!!!');
-  console.log('JOSH: ERROR!!!!');
-  console.log('JOSH: ERROR!!!!');
+  console.log('JOSH: Wrong Route!!!!');
   res.status(404);
   //   res.send('<h1>Page not found</h1>');
   res.sendFile(path.join(__dirname, 'views', '404.html'));
 });
 //===========================
-app.listen(3000);
+const port_num = 3e3;
+app.listen(port_num, () => console.log('http://localhost:3000'));
 //===========================
