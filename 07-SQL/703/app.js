6b82bf02129a5362b4c3ac58e9650aa95c674c81
JoshuaHolloway <jhollow6@asu.edu>
2020-02-17 18:37:16 -0600

    702 -> 703

---
+++ 07-SQL/703/app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// [702]
+// db.execute('SELECT * FROM products')
+//     .then(
+//         result => {
+//             console.log(result);
+//         }
+//     )
+//     .catch(
+//         err => {
+//             console.log(err);
+//         }
+//     );
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+const port_num = 3e3;
+app.listen(port_num, () => console.log('http://localhost:3000'));
